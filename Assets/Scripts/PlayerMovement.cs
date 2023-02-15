13ee1083a48c491cce4a54a8887b81106860d4eb
NeoTyger <neojl333@gmail.com>
2023-02-15 23:28:17 +0100

    Fisicas añadidas, lo que hay que corregir el que no se mueva, posible error del collider

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private CharacterController _controller;
+
+    // Velocidad de movimiento para el jugador
+    public float speed = 5.0f;
+    
+    // Variables para la garvedad
+    private Vector3 velocity;
+    public float gravity = -9.81f;
+    
+    // GroundCheck
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f; //Umbral de distancia del suelo
+    public LayerMask groundMask;
+    
+    // Jump
+    public float jumpHeight = 2f;
+
+    private void Start()
+    {
+        _controller = GameObject.FindWithTag("Player").GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Obtiene el movimiento horizontal y vertical
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calcula la dirección del movimiento
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        direction = transform.TransformDirection(direction);
+
+        // Calcula el movimiento completo multiplicando por la velocidad y el tiempo
+        transform.position += direction * speed * Time.deltaTime;
+        
+        // Gravedad
+        // Fórmula: velocidad = aceleración * tiempo ^ 2
+        velocity.y += gravity * Time.deltaTime;
+        _controller.Move(velocity * Time.deltaTime);
+        
+        // Mira si estoy tocando el suelo
+        isGrounded = Physics.CheckSphere(groundCheck.position,groundDistance,groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+    }
+}
