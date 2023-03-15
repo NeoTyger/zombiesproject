2aa59e29b5445623a1222fd01e3e272bf1b01fe8
NeoTyger <neojl333@gmail.com>
2023-03-15 10:24:56 +0100

    HUD implementada.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+
     public int enemiesAlive;
     public int round;
 
     public GameObject[] spawnPoints;
     public GameObject enemyPrefab;
+
+    public TextMeshProUGUI txtHp;
+    public TextMeshProUGUI txtRound;
 
     // Update is called once per frame
     void Update()
@@ -19,6 +23,9 @@ public class GameManager : MonoBehaviour
             round++;
             NextWave(round);
         }
+
+        txtHp.text = "HP : " + PlayerManager.health;
+        txtRound.text = "Round : " + round;
     }
 
     public void NextWave(int numRound)
@@ -26,7 +33,7 @@ public class GameManager : MonoBehaviour
         for (int num = 0; num <= numRound; num++)
         {
             int numRandom = Random.Range(0,5);
-            Debug.Log(numRandom);
+            //Debug.Log(numRandom);
             Instantiate(enemyPrefab,spawnPoints[numRandom].transform.position,Quaternion.identity);
             enemiesAlive++;
         }
