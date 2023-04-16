002e5e8b9f225d8c379167c7d753a192becea41b
NeoTyger <neojl333@gmail.com>
2023-04-17 00:56:55 +0200

    Video 2.1

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,26 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+
     public void Singleplayer()
     {
-        
+        SceneManager.LoadScene(2);
     }
-    
+
+    public void Multiplayer()
+    {
+        SceneManager.LoadScene(1);
+    }
+
     public void Exit()
     {
 #if UNITY_EDITOR
@@ -29,4 +36,10 @@ public class MainMenuManager : MonoBehaviour
         Application.Quit();
 #endif
     }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
--- Assets/Scripts/NetworkingManager.cs
+++ Assets/Scripts/NetworkingManager.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class NetworkingManager : MonoBehaviourPunCallbacks
 {
+    public Button btnMultiplayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,5 +34,39 @@ public class NetworkingManager : MonoBehaviourPunCallbacks
     public override void OnJoinedLobby()
     {
         Debug.Log("Preparado para jugar multijugador.");
+        btnMultiplayer.interactable = true;
+    }
+
+    public void FindMatch()
+    {
+        Debug.Log("Buscando sala...");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        MakeRoom();
+    }
+
+    private void MakeRoom()
+    {
+        int randomRoomNum = Random.Range(0, 5000);
+
+        RoomOptions roomOptions = new RoomOptions()
+        {
+            IsVisible = true,
+            IsOpen = true,
+            MaxPlayers = 6,
+            PublishUserId = true
+        };
+
+        PhotonNetwork.CreateRoom($"RoomName_{randomRoomNum}",roomOptions);
+        Debug.Log($"Sala creada : {randomRoomNum}");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Cargar escena del juego Multiplayer");
+        SceneManager.LoadScene(2);
     }
 }
